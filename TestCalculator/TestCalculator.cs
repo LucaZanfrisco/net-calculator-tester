e94864994ab0968690351f17cbff4bcb585cb01c
LucaZanfrisco <lzanfrisco9@gmail.com>
2023-09-25 16:19:04 +0200

    Aggiunte descrizioni test

--- TestCalculator/TestCalculator.cs
+++ TestCalculator/TestCalculator.cs
@@ -11,7 +11,7 @@ namespace TestCalculator
            newCalculator = new Calculator();
         }
 
-        [Test]
+        [Test(Description = "Test che controlla se il risultato della somma é un positivo")]
         [TestCase(3.5f,6.9f)]
         public void Test_AddResultMoreThenZero_Success(float num1, float num2)
         {
@@ -20,7 +20,7 @@ namespace TestCalculator
             Assert.True(result > 0,$"Test per la somma fallito: {result}");
         }
 
-        [Test]
+        [Test(Description ="Test che controlla se il risultato della somma sia corretto")]
         [TestCase(3.5f, 6.9f)]
         public void Test_AddReturnCorretValue_Succes(float num1, float num2)
         {
@@ -29,7 +29,7 @@ namespace TestCalculator
             Assert.AreEqual(result, (num1 + num2));
         }
 
-        [Test]
+        [Test(Description ="Test che controlla se il risultato della sottrazione sia un numero positivo")]
         [TestCase(23.89f, 45.12f)]
         public void Test_SubstractResultMoreThenZero_Success(float num1,float num2)
         {
@@ -38,14 +38,14 @@ namespace TestCalculator
             Assert.True(result > 0, "Il risultato è maggiore di zero");
         }
 
-        [Test]
+        [Test(Description ="Test che controlla se la divisione non generi nessuna eccezione")]
         [TestCase(23.89f, 2.5f)]
         public void Test_DivideNotThrowException_Success(float num1, float num2)
         {
             Assert.DoesNotThrow(() => { newCalculator.Divide(num1, num2); });
         }
 
-        [Test]
+        [Test(Description ="Test che controlla se il risultato della divisione non sia uguale a zero")]
         [TestCase(1f, 23.5f)]
         public void Test_DivideResultNotEqualsZero_Success(float num1, float num2)
         {
@@ -55,7 +55,7 @@ namespace TestCalculator
         }
 
 
-        [Test]
+        [Test(Description="Test che controlla se il risultato della moltiplicazione non sia zero")]
         [TestCase(10f, 23.5f)]
         public void Test_MultiplyResultNotEqualsZero_Success(float num1, float num2)
         {
@@ -64,7 +64,7 @@ namespace TestCalculator
             Assert.True(result > 0 || result < 0, "Il risultato della divisione è zero");
         }
 
-        [Test]
+        [Test(Description ="Test che controlla se la divisione non generi nessuna eccezione(quando viene passato zero come valore)")]
         [TestCase(10f, 2f)]
         public void Test_MultiplyNotThrowException(float num1, float num2)
         {
