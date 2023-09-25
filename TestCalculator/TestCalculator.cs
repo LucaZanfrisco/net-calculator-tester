1b173ed9408364379f82a7c6401000e06e8f5b14
LucaZanfrisco <lzanfrisco9@gmail.com>
2023-09-25 15:25:36 +0200

    Classe Calculator e primo test sulla somma

---
+++ TestCalculator/TestCalculator.cs
@@ -0,0 +1,27 @@
+using net_calculator_tester;
+
+namespace TestCalculator
+{
+    public class Tests
+    {
+        private Calculator newCalculator;
+        [SetUp]
+        public void Setup()
+        {
+           newCalculator = new Calculator();
+        }
+
+        [Test]
+        [TestCase(3.5f,6.9f)]
+        public void Test_AddMoreThenZero_Success(float num1, float num2)
+        {
+            float result = newCalculator.Add(num1, num2);
+
+            Assert.True(result > 0,$"Test per la somma superato: {result}");
+        }
+        
+
+
+
+    }
+}
--- net-calculator-tester/Calculator.cs
+++ net-calculator-tester/Calculator.cs
@@ -8,6 +8,11 @@ namespace net_calculator_tester
 {
     public class Calculator
     {
+        
+        public float Add(float num1, float num2)
+        {
+            return num1 + num2;
+        }
 
     }
 }
